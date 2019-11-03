2ee82e75afc7ae2fe6d206ee0f4884c64ae058ae
Klochkov Anton <tklochkov@gmail.com>
2019-11-03 14:30:20 +0300

    fixed bug with geo storage

--- lab2/src/redux/actions/actions.js
+++ lab2/src/redux/actions/actions.js
@@ -6,4 +6,4 @@ export const UPDATE_GEO_LOCATION = 'UPDATE_GEO_LOCATION';
 export const addNewFavoriteCity = name => ({ type: ADD_NEW_FAVORITE_CITY, name: name });
 export const removeFavoriteCity = name => ({ type: REMOVE_FAVORITE_CITY, name: name });
 export const addErrorModal = errorMessage => ({ type: ADD_ERROR_MODAL, errorMessage: errorMessage });
-export const updateGeoLocation = errorMessage => ({ type: UPDATE_GEO_LOCATION, errorMessage: errorMessage });
+export const updateGeoLocation = errorMessage => ({ type: UPDATE_GEO_LOCATION, coordinates: errorMessage });
